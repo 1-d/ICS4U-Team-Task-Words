6fbd1acae1a0d7483b355aa4787dce540cf88afe
1-d <jason_yuen2007@hotmail.com>
2016-11-04 14:07:21 -0400

    Add files via upload

--- WordBattle/WordBattle.java
+++ WordBattle/WordBattle.java
@@ -47,7 +47,16 @@ public class WordBattle extends World
     {    
         // Create a new world with 960x640 cells with a cell size of 1x1 pixels.
         super(960, 640, 1);
+        
+        prepare();
+    }
 
+    /**
+     * Prepare the world for the start of the program. That is: create the initial
+     * objects and add them to the world.
+     */
+    private void prepare()
+    {
         // create the helper text
         addObject(new HelperText("Welcome to WordBattle!"), 480, 545);
         addObject(new HelperText("Left: Speed up the animation"), 480, 570);
@@ -60,8 +69,6 @@ public class WordBattle extends World
             addObject(tempWord,0,0);
             words.add(tempWord);
         }
-
-        prepare();
     }
 
     /**
@@ -162,13 +169,5 @@ public class WordBattle extends World
                 timeLeft = PHASE_0_LENGTH;
             }
         }
-    }
-
-    /**
-     * Prepare the world for the start of the program. That is: create the initial
-     * objects and add them to the world.
-     */
-    private void prepare()
-    {
     }
 }
